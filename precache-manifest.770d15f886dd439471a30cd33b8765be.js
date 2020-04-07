16cdb9efa337fd0dfc0801a7cd11a5372ca8ae4e
bob bae <bob.bae@gmail.com>
2020-04-07 16:14:33 -0700

    Updates

---
+++ precache-manifest.770d15f886dd439471a30cd33b8765be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86ac961ba2c9bb28ec10f3e564e96e7d",
+    "url": "/reactspa/index.html"
+  },
+  {
+    "revision": "276e7ca1dee02d795261",
+    "url": "/reactspa/static/css/main.9a336252.chunk.css"
+  },
+  {
+    "revision": "bfb0123665612980dbd9",
+    "url": "/reactspa/static/js/2.d4248eac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactspa/static/js/2.d4248eac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "276e7ca1dee02d795261",
+    "url": "/reactspa/static/js/main.1d1d3b85.chunk.js"
+  },
+  {
+    "revision": "66157ef9551119060e37",
+    "url": "/reactspa/static/js/runtime-main.9fe8c4d1.js"
+  }
+]);
